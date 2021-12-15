fa16ad09bf2d5b2c3c37669f9afb7c63a55ecb61
HongweiJi <17865320564@163.com>
2021-12-15 18:57:10 +0800

    增加课程3

--- readme.m
+++ readme.m
@@ -8,15 +8,23 @@
 使用命令git commit -m <message>，完成。
 
 
+课程2：版本变更（回到过去，穿梭未来）
+HEAD指向的版本就是当前版本，因此，Git允许我们在版本的历史之间穿梭，使用命令git reset --hard commit_id。
 
-课程2：
+穿梭前，用git log可以查看提交历史，以便确定要回退到哪个版本。
+
+要重返未来，用git reflog查看命令历史，以便确定要回到未来的哪个版本。
+
+
+
+课程3：
 要随时掌握工作区的状态，例如文件已经add但是没有commit，使用git status命令可以看到提示。
 
 如果git status告诉你有文件被修改过，用 git diff 文件名 可以查看修改内容。 
 
 
 
-课程3
+课程4
 HEAD指向的版本就是当前版本，因此，Git允许我们在版本的历史之间穿梭，使用命令git reset --hard commit_id。
 
 穿梭前，用git log可以查看提交历史，以便确定要回退到哪个版本。
